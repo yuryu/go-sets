5f6960fdd755df5a0bcefce93441bbf3b24234fa
M. J. Fromberger <michael.j.fromberger@gmail.com>
2014-08-01 15:44:33 -0700

    Add stringset package.
    This is an lightweight implementation of a set of strings, based on the Go
    built-in map type, that supports some common set-like operations.

---
+++ stringset.go
@@ -0,0 +1,188 @@
+// Package stringset implements a lightweight set-of-strings type based on Go's
+// built-in map type.  A Set provides some convenience methods for common set
+// operations.
+//
+// Example:
+//    one := stringset.New("one")
+//    none := one.Intersect(nil)
+package stringset
+
+import (
+	"sort"
+	"strconv"
+	"strings"
+)
+
+// A Set represents a set of string values.  A nil Set is a valid
+// representation of an empty set.
+type Set map[string]struct{}
+
+// String implements the fmt.Stringer interface.
+func (s Set) String() string {
+	if s.Empty() {
+		return "ø"
+	}
+	keys := make([]string, len(s))
+	for i, k := range s.Keys() {
+		keys[i] = strconv.Quote(k)
+	}
+	return "{" + strings.Join(keys, ", ") + "}"
+}
+
+// New returns a new set containing exactly the specified elements.  Returns
+// nil if no elements are specified.
+func New(ss ...string) Set {
+	if len(ss) == 0 {
+		return nil
+	}
+	set := make(Set)
+	for _, s := range ss {
+		set[s] = struct{}{}
+	}
+	return set
+}
+
+// Len returns the number of elements in the set.
+func (s Set) Len() int { return len(s) }
+
+// Keys returns a lexicographically sorted slice of the elements in the set.
+func (s Set) Keys() []string {
+	var keys []string
+	for k := range s {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+// Contains reports whether the set contains the given string.
+func (s Set) Contains(str string) bool {
+	_, ok := s[str]
+	return ok
+}
+
+// IsSubset reports whether s1 is a subset of s2.
+func (s1 Set) IsSubset(s2 Set) bool {
+	if s1.Empty() {
+		return true
+	} else if s2.Empty() {
+		return false
+	}
+	for k := range s1 {
+		if _, ok := s2[k]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+// Equals reports whether s1 is equal to s2, having exactly the same elements.
+func (s1 Set) Equals(s2 Set) bool { return s1.IsSubset(s2) && s2.IsSubset(s1) }
+
+// Empty reports whether the set is empty.
+func (s Set) Empty() bool { return len(s) == 0 }
+
+// Union constructs the union s1 ∪ s2.
+func (s1 Set) Union(s2 Set) Set {
+	if s1.Empty() {
+		return s2
+	} else if s2.Empty() {
+		return s1
+	}
+	set := make(Set)
+	for k := range s1 {
+		set[k] = struct{}{}
+	}
+	for k := range s2 {
+		set[k] = struct{}{}
+	}
+	return set
+}
+
+// Intersect constructs the intersection s1 ∩ s2.
+func (s1 Set) Intersect(s2 Set) Set {
+	if s1.Empty() || s2.Empty() {
+		return nil
+	}
+	var set Set
+	for k := range s1 {
+		if _, ok := s2[k]; ok {
+			if set == nil {
+				set = make(Set)
+			}
+			set[k] = struct{}{}
+		}
+	}
+	return set
+}
+
+// Diff constructs the set difference s1 \ s2.
+func (s1 Set) Diff(s2 Set) Set {
+	if s1.Empty() || s2.Empty() {
+		return s1
+	}
+	var set Set
+	for k := range s1 {
+		if _, ok := s2[k]; !ok {
+			if set == nil {
+				set = make(Set)
+			}
+			set[k] = struct{}{}
+		}
+	}
+	return set
+}
+
+// Update adds the elements of s2 to *s1 in-place. If *s1 == nil a new set is
+// allocated that is a copy of s2.  Reports whether anything was added.
+func (s1 *Set) Update(s2 Set) bool {
+	in := len(*s1)
+	if *s1 == nil {
+		*s1 = make(Set)
+	}
+	for k := range s2 {
+		(*s1)[k] = struct{}{}
+	}
+	return len(*s1) == in
+}
+
+// Add adds the specified strings to *set in-place.  If *set == nil, allocates
+// a new set equivalent to New(ss...).  Reports whether anything was added.
+func (set *Set) Add(ss ...string) bool {
+	in := len(*set)
+	if *set == nil {
+		*set = make(Set)
+	}
+	for _, s := range ss {
+		(*set)[s] = struct{}{}
+	}
+	return len(*set) == in
+}
+
+// Remove removes the elements of s2 from s1 in-place.  Reports whether
+// anything was removed.
+//
+// Equivalent to s1 = s1.Diff(s2), but does not allocate a new set.
+func (s1 Set) Remove(s2 Set) bool {
+	in := s1.Len()
+	if !s1.Empty() {
+		for k := range s2 {
+			delete(s1, k)
+		}
+	}
+	return s1.Len() != in
+}
+
+// Discard removes the elements of ss from set in-place.  Reports whether
+// anything was removed.
+//
+// Equivalent to set = set.Diff(New(ss...)), but does not allocate a set for ss.
+func (set Set) Discard(ss ...string) bool {
+	in := set.Len()
+	if !set.Empty() {
+		for _, s := range ss {
+			delete(set, s)
+		}
+	}
+	return set.Len() != in
+}
---
+++ stringset_test.go
@@ -0,0 +1,234 @@
+package stringset
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestEmptiness(t *testing.T) {
+	var s Set
+	if !s.Empty() {
+		t.Errorf("nil Set is not reported empty: %v", s)
+	}
+
+	s = New()
+	if !s.Empty() {
+		t.Errorf("Empty Set is not reported empty: %v", s)
+	}
+	if s != nil {
+		t.Errorf("New() returned non-nil: %v", s)
+	}
+
+	s = New("something")
+	if s.Empty() {
+		t.Errorf("Nonempty Set is reported empty: %v", s)
+	}
+}
+
+func TestUniqueness(t *testing.T) {
+	// Sets should not contain duplicates.  Obviously this is impossible with
+	// the map implementation, but other representations are viable.
+	s := New("e", "a", "a", "c", "a", "b", "d", "c", "a", "e", "d", "e", "c", "a")
+	if got, want := s.Len(), 5; got != want {
+		t.Errorf("s.Len(): got %d, want %d [%v]", got, want, s)
+	}
+
+	// Keys should come out sorted.
+	wantKeys := []string{"a", "b", "c", "d", "e"}
+	if got := s.Keys(); !reflect.DeepEqual(got, wantKeys) {
+		t.Errorf("s.Keys(): got %+q, want %+q", got, wantKeys)
+	}
+}
+
+func TestMembership(t *testing.T) {
+	const count = 5
+	words := []string{
+		"alpha", "bravo", "charlie", "delta", "echo", "foxtrot",
+		"golf", "hotel", "india", "juliet", "kilo", "lima",
+	}
+	s := New(words[:count]...)
+	for i, w := range words {
+		if got, want := s.Contains(w), i < count; got != want {
+			t.Errorf("s.Contains(%q): got %v, want %v", w, got, want)
+		}
+	}
+}
+
+func TestIsSubset(t *testing.T) {
+	var empty Set
+	key := New("some", "of", "what", "a", "fool", "thinks", "often", "remains")
+	for _, test := range [][]string{
+		{}, {"of", "a"}, {"some", "what", "fool"},
+	} {
+		probe := New(test...)
+		if !probe.IsSubset(key) {
+			t.Errorf("IsSubset %+q ⊂ %+q is false", probe, key)
+		}
+		if !empty.IsSubset(probe) { // ø is a subset of everything, including itself.
+			t.Errorf("IsSubset ø ⊂ %+q is false", probe)
+		}
+	}
+}
+
+func TestNotSubset(t *testing.T) {
+	tests := []struct {
+		probe, key Set
+	}{
+		{New("a"), New()},
+		{New("a"), New("b")},
+		{New("a", "b"), New("b")},
+		{New("a", "c", "b"), New("a", "c", "d")},
+	}
+	for _, test := range tests {
+		if test.probe.IsSubset(test.key) {
+			t.Errorf("IsSubset %v ⊂ %v is true", test.probe, test.key)
+		}
+	}
+}
+
+func TestUnion(t *testing.T) {
+	vowels := New("e", "o", "i", "a", "u")
+	vkeys := []string{"a", "e", "i", "o", "u"}
+
+	consonants := New("h", "f", "b", "d", "g", "c")
+
+	if got := vowels.Union(nil).Keys(); !reflect.DeepEqual(got, vkeys) {
+		t.Errorf("Vowels ∪ ø: got %+q, want %+q", got, vkeys)
+	}
+	if got := New().Union(vowels).Keys(); !reflect.DeepEqual(got, vkeys) {
+		t.Errorf("ø ∪ Vowels: got %+q, want %+q", got, vkeys)
+	}
+
+	want := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "o", "u"}
+	if got := vowels.Union(consonants).Keys(); !reflect.DeepEqual(got, want) {
+		t.Errorf("Vowels ∪ Consonants: got %+q, want %+q", got, want)
+	}
+}
+
+func TestIntersect(t *testing.T) {
+	empty := New()
+	nat := New("0", "1", "2", "3", "4", "5", "6", "7")
+	odd := New("1", "a", "3", "5", "7", "p", "q")
+	prime := New("2", "m", "3", "d", "x", "5", "7", "!")
+
+	tests := []struct {
+		left, right Set
+		want        []string
+	}{
+		{empty, empty, nil},
+		{empty, nat, nil},
+		{nat, empty, nil},
+		{nat, nat, []string{"0", "1", "2", "3", "4", "5", "6", "7"}},
+		{nat, odd, []string{"1", "3", "5", "7"}},
+		{odd, nat, []string{"1", "3", "5", "7"}},
+		{odd, prime, []string{"3", "5", "7"}},
+		{prime, nat, []string{"2", "3", "5", "7"}},
+	}
+	for _, test := range tests {
+		got := test.left.Intersect(test.right).Keys()
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("%v ∩ %v: got %+q, want %+q", test.left, test.right, got, test.want)
+		}
+	}
+}
+
+func TestDiff(t *testing.T) {
+	empty := New()
+	nat := New("0", "1", "2", "3", "4", "5", "6", "7")
+	odd := New("1", "a", "3", "5", "7", "p", "q")
+	prime := New("2", "m", "3", "d", "x", "5", "7", "!")
+
+	tests := []struct {
+		left, right Set
+		want        []string
+	}{
+		{empty, empty, nil},
+		{empty, nat, nil},
+		{nat, empty, []string{"0", "1", "2", "3", "4", "5", "6", "7"}},
+		{nat, nat, nil},
+		{nat, odd, []string{"0", "2", "4", "6"}},
+		{odd, nat, []string{"a", "p", "q"}},
+		{odd, prime, []string{"1", "a", "p", "q"}},
+		{prime, nat, []string{"!", "d", "m", "x"}},
+	}
+	for _, test := range tests {
+		got := test.left.Diff(test.right).Keys()
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("%v \\ %v: got %+q, want %+q", test.left, test.right, got, test.want)
+		}
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	tests := []struct {
+		before, update Set
+		want           []string
+	}{
+		{nil, nil, nil},
+		{nil, New("a"), []string{"a"}},
+		{New("pdq"), nil, []string{"pdq"}},
+		{New("a", "b"), New("c", "c", "b"), []string{"a", "b", "c"}},
+	}
+	for _, test := range tests {
+		test.before.Update(test.update)
+		if got := test.before.Keys(); !reflect.DeepEqual(got, test.want) {
+			t.Errorf("Update %v: got %+q, want %+q", test.before, got, test.want)
+		}
+	}
+}
+
+func TestAdd(t *testing.T) {
+	tests := []struct {
+		before       Set
+		update, want []string
+	}{
+		{nil, nil, nil},
+		{nil, []string{"a"}, []string{"a"}},
+		{New("pdq"), nil, []string{"pdq"}},
+		{New("a", "b"), []string{"c", "c", "b"}, []string{"a", "b", "c"}},
+	}
+	for _, test := range tests {
+		test.before.Add(test.update...)
+		if got := test.before.Keys(); !reflect.DeepEqual(got, test.want) {
+			t.Errorf("Update %v: got %+q, want %+q", test.before, got, test.want)
+		}
+	}
+}
+
+func TestRemove(t *testing.T) {
+	tests := []struct {
+		before, update Set
+		want           []string
+	}{
+		{nil, nil, nil},
+		{nil, New("a"), nil},
+		{New("pdq"), nil, []string{"pdq"}},
+		{New("a", "b"), New("c", "c", "b"), []string{"a"}},
+		{New("a", "b", "c"), New("d", "e"), []string{"a", "b", "c"}},
+	}
+	for _, test := range tests {
+		test.before.Remove(test.update)
+		if got := test.before.Keys(); !reflect.DeepEqual(got, test.want) {
+			t.Errorf("Remove %v: got %+q, want %+q", test.before, got, test.want)
+		}
+	}
+}
+
+func TestDiscard(t *testing.T) {
+	tests := []struct {
+		before       Set
+		update, want []string
+	}{
+		{nil, nil, nil},
+		{nil, []string{"a"}, nil},
+		{New("pdq"), nil, []string{"pdq"}},
+		{New("a", "b"), []string{"c", "c", "b"}, []string{"a"}},
+		{New("a", "b", "c"), []string{"d", "e"}, []string{"a", "b", "c"}},
+	}
+	for _, test := range tests {
+		test.before.Discard(test.update...)
+		if got := test.before.Keys(); !reflect.DeepEqual(got, test.want) {
+			t.Errorf("Remove %v: got %+q, want %+q", test.before, got, test.want)
+		}
+	}
+}
